529a6a4459f2707d084918032a4584e10da3e5e3
eropick <34877404+eropick@users.noreply.github.com>
2025-01-01 11:37:54 +0900

    forked-release-2024-09-25

--- source/MultiRoom_server/src/module-info.java
+++
@@ -1,8 +0,0 @@
-open module MultiRoomServer {
-	requires json.simple;
-	requires org.json;
-	requires java.sql; //JDBC
-	requires org.slf4j; //SLF4J
-	requires com.zaxxer.hikari; //HikariCP
-	requires java.desktop; 
-}
